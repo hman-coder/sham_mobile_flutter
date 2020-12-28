502f6477f24ccf4575d1682294c4c5dff2b8948b
HyldenMan <swissknifesoft@gmail.com>
2020-12-28 14:10:41 +0200

    default BookTile.onTap now uses Getx

--- lib/widgets/book_tile.dart
+++ lib/widgets/book_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:sham_mobile/book/book_controller.dart';
 import 'package:sham_mobile/book/view_book_ui.dart';
 import 'package:sham_mobile/models/book.dart';
 
@@ -16,7 +17,11 @@ class BookTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(book.title),
-      onTap: onTap ?? () => Get.to(ViewBookUI(book: book)),
+      onTap: onTap ?? () => Get.to(GetBuilder(
+        init: BookController(book),
+        builder: (controller) => ViewBookUI()
+      )
+      ),
       subtitle: Text(book.authorsAsString),
       leading: Image.asset(book.image),
       trailing: trailing
