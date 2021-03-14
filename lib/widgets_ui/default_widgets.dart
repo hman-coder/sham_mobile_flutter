c6e7bfe1fefaeadc3e4a0d56662cafd0c8bbc1dd
HyldenMan <swissknifesoft@gmail.com>
2021-03-14 05:43:32 +0200

    added default_widgets.dart

---
+++ lib/widgets_ui/default_widgets.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const Widget verticalTinySpacer = const SizedBox(height: 5,);
+const Widget verticalSmallSpacer = const SizedBox(height: 10,);
+const Widget verticalMediumSpacer = const SizedBox(height: 15,);
+const Widget verticalLargeSpacer = const SizedBox(height: 20,);
+const Widget verticalHugeSpacer = const SizedBox(height: 25,);
+
+const Widget horizontalTinySpacer = const SizedBox(width: 5,);
+const Widget horizontalSmallSpacer = const SizedBox(width: 10,);
+const Widget horizontalMediumSpacer = const SizedBox(width: 15,);
+const Widget horizontalLargeSpacer = const SizedBox(width: 20,);
+const Widget horizontalHugeSpacer = const SizedBox(width: 25,);
