738f44f90e063b54865e1973cb2ce69b31724ea8
hman-coder <hman_codes@protonmail.com>
2021-04-09 10:20:09 +0300

    added no_items_to_show_ui.dart
    * Used in views where there should be lists but there are no items to show.

---
+++ lib/ui/no_items_to_show_ui.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:sham_mobile/constants/default_values.dart';
+import 'package:get/utils.dart';
+
+/// Used in views where there should be lists but there are no items to show.
+/// It shows a background, fading image, with a text below it.
+class NoItemsToShowUI extends StatelessWidget {
+  /// The text to show. If this value is null, then ['no_items_to_show'.tr]
+  /// is used.
+  final String message;
+
+  /// Image asset path
+  final String imagePath;
+
+  NoItemsToShowUI({Key key, this.message, this.imagePath = 'assets/images/sham_logo.png'}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+
+        children: [
+          // Without Row + Expanded the image wouldn't stretch
+          Row(
+            children: [
+              Expanded(
+                child: Opacity(
+                  opacity: 0.3,
+                  child: Image.asset(
+                    imagePath,
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          IntrinsicWidth(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 12),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                border: Border.all(),
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Text(message ?? 'no_items_to_show'.tr, style: Theme.of(context).textTheme.headline6.copyWith(color: kcMaroon)),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
